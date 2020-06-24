ea3ec9211ac6d6e748ce49ce15700c56d7769cae
5aitama サイタマ <44070842+5aitama@users.noreply.github.com>
2020-06-24 11:53:19 +0200

    Update NormalSolver.cs

--- Runtime/NormalSolver.cs
+++ Runtime/NormalSolver.cs
@@ -1,8 +1,9 @@
 /* 
  * ============================================================================================================ *
- * The following code was a modification                                                                        |
- * of the code from : https://schemingdeveloper.com/2017/03/26/better-method-recalculate-normals-unity-part-2/  |
- * Special thank to him for his work !! <3                                                                      |
+ * The following code was an adaptation of the code from :                                                      |
+ * https://schemingdeveloper.com/2017/03/26/better-method-recalculate-normals-unity-part-2/                     |
+ * to Unity Jobs friendly code.                                                                                 |
+ * Special thank to him for his incredible work !! <3                                                           |
  * ============================================================================================================ *
  */
 
@@ -15,7 +16,13 @@ namespace Saitama.ProceduralMesh
 {
     public static class NormalSolver
     {
-        public static void RecalculateNormals(NativeArray<Vertex> vert, NativeArray<Triangle> tris, ref NativeArray<float3> normals, float angle)
+        /// <summary>
+        /// Smooth mesh.
+        /// </summary>
+        /// <param name="vert">Vertex array that normals would be stored in</param>
+        /// <param name="tris">Triangle array</param>
+        /// <param name="angle">Smoothing angle</param>
+        public static void RecalculateNormals(ref NativeArray<Vertex> vert, NativeArray<Triangle> tris, float angle = 60)
         {
             var cosineThreshold = Mathf.Cos(angle * Mathf.Deg2Rad);
             var triNormals = new NativeArray<float3>(tris.Length, Allocator.Temp);
@@ -65,8 +72,73 @@ namespace Saitama.ProceduralMesh
                                 sum += triNormals[rhs.TriangleIndex];
                         }
                     } while(enumerator2.MoveNext());
+                    
+                    vert[lhs.VertexIndex].norm = math.normalize(sum);
 
-                    normals[lhs.VertexIndex] = math.normalize(sum);
+                } while(enumerator1.MoveNext());
+            }
+        }
+        
+        /// <summary>
+        /// Smooth mesh.
+        /// </summary>
+        /// <param name="vert">Vertex array</param>
+        /// <param name="tris">Triangle array</param>
+        /// <param name="normals">Array that normals would be stored in</param>
+        /// <param name="angle">Smoothing angle</param>
+        public static void RecalculateNormals(NativeArray<Vertex> vert, NativeArray<Triangle> tris, ref NativeArray<float3> normals, float angle = 60)
+        {
+            var cosineThreshold = Mathf.Cos(angle * Mathf.Deg2Rad);
+            var triNormals = new NativeArray<float3>(tris.Length, Allocator.Temp);
+            var dictionary = new NativeMultiHashMap<VertexKey, VertexEntry>(vert.Length * 2, Allocator.Temp);
+
+            for(var i = 0; i < tris.Length; i++)
+            {
+                var i1 = tris[i].indices[0];
+                var i2 = tris[i].indices[1];
+                var i3 = tris[i].indices[2];
+
+                // Calculate the normal of the triangle
+                var p1 = vert[i2].pos - vert[i1].pos;
+                var p2 = vert[i3].pos - vert[i1].pos;
+                var normal = math.normalize(math.cross(p1, p2));
+
+                triNormals[i] = normal;
+                
+                dictionary.Add(new VertexKey(vert[i1].pos), new VertexEntry(0, i, i1));
+                dictionary.Add(new VertexKey(vert[i2].pos), new VertexEntry(0, i, i2));
+                dictionary.Add(new VertexKey(vert[i3].pos), new VertexEntry(0, i, i3));
+            }
+
+            var keys = dictionary.GetKeyArray(Allocator.Temp);
+
+            for(var i = 0; i < keys.Length; i++)
+            {
+                var enumerator1 = dictionary.GetValuesForKey(keys[i]);
+                do
+                {
+                    var sum = new float3();
+                    var lhs = enumerator1.Current;
+                    var enumerator2 = dictionary.GetValuesForKey(keys[i]);
+                    do
+                    {
+                        var rhs = enumerator2.Current;
+
+                        if (lhs.VertexIndex == rhs.VertexIndex) 
+                            sum += triNormals[rhs.TriangleIndex];
+                        else 
+                        {
+                            // The dot product is the cosine of the angle between the two triangles.
+                            // A larger cosine means a smaller angle.
+                            var dot = math.dot(triNormals[lhs.TriangleIndex], triNormals[rhs.TriangleIndex]);
+
+                            if (dot >= cosineThreshold)
+                                sum += triNormals[rhs.TriangleIndex];
+                        }
+                    } while(enumerator2.MoveNext());
+                    
+                    vert[lhs.VertexIndex].norm = math.normalize(sum);
+                    //normals[lhs.VertexIndex] = math.normalize(sum);
 
                 } while(enumerator1.MoveNext());
             }
